578a3550253ec1e3175c6e2d28a6828b1edc2a47
iniadewumi <72786828+iniadewumi@users.noreply.github.com>
2022-01-20 19:50:49 -0600

    trained neural network

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import cv2
-import pathlib, pickle
-import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import svm
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from cleaning_preprocess import ImageProcessor
 
+import pathlib
+from cleaning_preprocess import ImageProcessor
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras import models
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 
@@ -20,91 +15,43 @@ PARASITISED = IMG_DIR / 'Parasitized'
 UNINFECTED = IMG_DIR / 'Uninfected'
 MODELS = HOME / 'Models'
 
+img_width, img_height = 139, 139
+batch_size = 16
 
-def latest_file(path=MODELS, model_type="RandomForest"):
-    files = [x for x in path.glob("*.pickle") if model_type in str(x)]
+
+def latest_file(path=MODELS, model_type="NeuralNetwork"):
+    files = [x for x in path.glob("*.h5") if model_type in str(x)]
     return max(files, key=lambda x: x.stat().st_ctime)
 
-class MalariaModel():
-    def __init__(self, train=False, random_state=None):
-        self.df = pd.read_csv(DATASETS / "malaria_dataset.csv")
-        self.df = self.df.sort_values("Label")
-        self.y_cols = "Label"
-        self.x_cols = [x for x in self.df.columns if x!=self.y_cols]
-
-        self.X = self.df[self.x_cols]
-        self.y = self.df[self.y_cols]
-
-
-        if train:
-            self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
-            self.train_model()
-        else:
-            print("Since train == False, latest model will be loaded!")
-            model_to_load = input("Enter Model name to load, or press Enter for latest model: ")
-            if model_to_load=="":
-                self.load_model()
-                return  print("Loaded the latest model")
-            try:
-                self.trained_model = self.load_model(model_name=MODELS/model_to_load)
-                return print("Model loaded Successfully")
-            except Exception as e:
-                print(e)
-                return print("Failed to load model! Check model name... Models can be found in Models folder.")
-            
-    def train_model(self,):
-        classifier_type=input("What classifier would you like to use for training? (RandomForest, KNN, SVM): ")
-        print(f"Training model with {classifier_type}...")
-        if classifier_type == "SVM": 
-            print("Please Note that SVM will take significantly longer to train than other classifiers.")
-        classifiers = {"RandomForest": RandomForestClassifier(max_depth=5), "KNN": KNeighborsClassifier(n_neighbors=18), "SVM": svm.SVC()}
+class NNMalariaModel:
+    def __init__(self):
+        pass
         
-        self.trained_model = classifiers[classifier_type] #RandomForestClassifier(max_depth=5)
-        self.trained_model.fit(self.x_train, self.y_train)
-        self.predictions = self.trained_model.predict(self.x_test)
-        print(classification_report(self.predictions, self.y_test))
-        save_or_not = input("Save Model? (y or n) ")
-        if save_or_not == "y":
-            models_count = sum(x.suffix==".pickle" and classifier_type in x.name for x in MODELS.iterdir())
-            model_name = MODELS/f"{classifier_type}({models_count}).pickle"
-            self.save_model(self.trained_model, model_name)
-            print(f"\n\nModel saved as {model_name.name}")
-        else:
-            print("Model not saved")
-        
-
-    def save_model(self, model, model_name):
-        with open(model_name, "wb") as f:
-            pickle.dump(model, f)
-
-    def load_model(self, model_name=None):
-        """
-        If not model_name is provided, the latest model will be loaded.
-
-        Args:
-            model_name ([type], optional): [description]. Defaults to None.
-        """
-        model_type = input("What classifier do you want to load? (RandomForest, KNN or SVM) Default='RandomForest': ")
-        print(f"Loading latest {model_type} model...")
-        if not model_name:
-            model_name = str(latest_file())
-        with open(model_name, "rb") as f:
-            self.trained_model = pickle.load(f)            
-            
     def has_malaria(self):
-        # Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
         out = ["Infected", "Uninfected"]
         filename = askopenfilename(initialdir = str(HOME), title = "Select Malaria cell image", filetypes = (("png files","*.png"),("jpeg files","*.jpg")))
-        inp = ImageProcessor().single_image(pathlib.Path(filename))
+        img = load_img(filename)
         
-        results = self.trained_model.predict_proba(inp)
+        test_datagen = ImageDataGenerator(rescale=1. / 255)
+        x = img_to_array(img)
+        x = x.reshape((1,) + x.shape)
+        inp = test_datagen.flow(x)
+        
+        
+        results = self.model.predict(inp)
         print(results.argmax())
         final = f"\n\nPrediction: {out[results.argmax()]}\nProbability: {results.max()}\n\n"
         return print(final)
-        
+    
+    
+    def load_model(self):
+        print("Loading latest Neural Network model...")
+        model_name = str(latest_file())
+        self.model = models.load_model(model_name)       
+            
         
 if __name__ == "__main__":
-    malaria_model = MalariaModel(train=True)
+    malaria_model = NNMalariaModel()
     malaria_model.has_malaria()
     while True:
         cont = input("Predict New? (y or n)")
